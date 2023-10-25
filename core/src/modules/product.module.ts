061acd7a3e9103f373221715cee4736c1b187d0f
Anderson Raphael Ferreira <phaell.anderson09@gmail.com>
2023-10-25 17:07:56 -0300

    feat: adicionado provider de product

--- core/src/modules/product.module.ts
+++ core/src/modules/product.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { productProviders } from '@providers/product.providers';
 import { DatabaseModule } from './database.module';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [...productProviders],
 })
 export class ProductModule {}
