20141b9033727d76f7242fb5ca6f49e54998aa82
Anderson Raphael Ferreira <phaell.anderson09@gmail.com>
2023-10-30 14:55:27 -0300

    fix: alterado monitoramento de logs do console para o Logger

--- image_process/src/app.service.ts
+++ image_process/src/app.service.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { S3 } from 'aws-sdk';
 import axios from 'axios';
-import * as fs from 'fs';
-import { join } from 'path';
 import * as sharp from 'sharp';
 import { v4 } from 'uuid';
 
@@ -14,6 +12,8 @@ export class AppService {
   ) {}
   uploadedImageUrl: string;
 
+  private readonly logger = new Logger(AppService.name);
+
   s3 = new S3({
     region: this.configService.get('AWS_S3_REGION'),
     credentials: {
@@ -23,58 +23,22 @@ export class AppService {
   });
 
   async processImage(productId: number, image_url: string) {
-    const originalImageLocation = join(__dirname, 'temp', `${v4()}.jpg`);
-
-    const newFileLocation = join(__dirname, 'temp', `${v4()}-compressed.jpg`);
-
     try {
       const { data } = await axios.get(image_url, {
         responseType: 'arraybuffer',
-      });
-
-      await new Promise<void>((resolve) => {
-        fs.writeFile(
-          originalImageLocation,
-          data,
-          (err: NodeJS.ErrnoException) => {
-            if (err) {
-              console.log(
-                'erro ao criar imagem com retorno do AXIOS: ',
-                err.message,
-              );
-            }
-
-            resolve();
-          },
-        );
-      });
-
-      await new Promise<void>((resolve): void => {
-        sharp(fs.readFileSync(originalImageLocation))
-          .jpeg({ quality: 80 })
-          .toFile(newFileLocation, (err: Error) => {
-            if (err) {
-              console.log('erro no SHARP: ', err.message);
-              return { productId, error: true };
-            }
-
-            resolve();
-          });
       });
 
       const { Location } = await this.s3
         .upload({
           Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
           Key: `${v4()}.jpg`,
-          Body: fs.readFileSync(newFileLocation),
+          Body: sharp(data).jpeg({ quality: 80 }).toBuffer(),
         })
         .promise();
 
       this.uploadedImageUrl = Location;
-
-      fs.unlinkSync(originalImageLocation);
-      fs.unlinkSync(newFileLocation);
     } catch (error) {
+      this.logger.log(`AppService catch error: ${error.message}`);
       return { productId, error: true };
     }
 
--- image_process/src/main.ts
+++ image_process/src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
@@ -5,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
 
   const configService = app.get(ConfigService); // Acesse o ConfigService diretamente
 
@@ -22,7 +24,7 @@ async function bootstrap() {
   (await app.startAllMicroservices()).listen(
     configService.get<string>('APP_PORT') || 3001,
     () => {
-      console.log(
+      logger.log(
         `Server is listening on ${
           configService.get<string>('APP_PORT') || 3001
         }`,
